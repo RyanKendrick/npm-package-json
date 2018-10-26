348e31f569d715aea15f86631f65ef130eb4e543
Ryan Kendrick <rpkendrick@gmail.com>
2018-10-26 17:24:32 +0000

    module that exports a function that takes a number as a parameter and stores it in a list, another function that returns a version of the data list that is sorted in ascending order, also impleented a a Node.js script that imports the functionality of the module, adds 5 different data points to the data list and outputs the sorted list

---
+++ custom-sorting-module-import.js
@@ -0,0 +1,9 @@
+var theList = require("./custom-sorting-module");
+
+
+theList.storeList(7);
+theList.storeList(78);
+theList.storeList(9);
+theList.storeList(8);
+theList.storeList(3);
+theList.sortList();
